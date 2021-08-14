85d07fd42376bbe49cee18108e5c2a2ac028f2d0
saintkim12 <saintkim1232@gmail.com>
2021-08-14 17:43:14 +0900

    build 설정

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import mdPlugin from 'vite-plugin-markdown'
 
 module.exports = {
-  plugins: [mdPlugin({ mode: 'html' })]
+  plugins: [mdPlugin({ mode: 'html' })],
+  build: {
+    outDir: '../docs',
+    emptyOutDir: true
+  }
 }
